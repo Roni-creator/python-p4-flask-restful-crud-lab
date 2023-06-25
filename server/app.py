f14fbc979f96142ff52875d2d7e57af503dc7317
veronicahmbuvi <mbuviveronicah3@gmail.com>
2023-06-25 22:13:31 +0300

    completed assignment

--- server/app.py
+++ server/app.py
@@ -17,19 +17,17 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-
     def get(self):
         plants = [plant.to_dict() for plant in Plant.query.all()]
         return make_response(jsonify(plants), 200)
 
     def post(self):
-
         data = request.get_json()
 
         new_plant = Plant(
             name=data['name'],
             image=data['image'],
-            price=data['price'],
+            price=data['price']
         )
 
         db.session.add(new_plant)
@@ -40,13 +38,29 @@ class Plants(Resource):
 api.add_resource(Plants, '/plants')
 
 class PlantByID(Resource):
-
     def get(self, id):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
 
+    def patch(self, id):
+        data = request.get_json()
+        plant = Plant.query.filter(Plant.id == id).first()
+        for attr in data:
+            setattr(plant, attr, data[attr])
+        db.session.add(plant)
+        db.session.commit()
+        response_dict = plant.to_dict()
+        response = make_response(response_dict, 200)
+        return response
+
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        db.session.delete(plant)
+        db.session.commit()
+        return make_response('', 204)
+
 api.add_resource(PlantByID, '/plants/<int:id>')
-        
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
